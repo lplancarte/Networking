b86b22bc4c4c152c5ce398d1b30a6f0106b9c079
Lucio Plancarte <lplancarte@cnm.edu>
2024-11-23 17:12:19 -0700

    Changed colors on GUI added vertical Scroll bar to output text area

--- Client.java
+++ Client.java
@@ -23,6 +23,9 @@ import javax.swing.BoxLayout;
 import java.awt.Component;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JScrollBar;
+
+import java.awt.Font;
 
 public class Client extends JFrame{
 
@@ -43,73 +46,94 @@ public class Client extends JFrame{
 	private JPanel displayPanel3; //matrix 3
 
 	private JLabel fileInputLabel;
+	private JLabel fileNotFoundLbl;
 
 	private JTextArea output;
+	private JScrollPane outputPanel; //terminal Output
 
 	private File file;
 	private Scanner inputScanner;
 
+	/*private String dummyData =  "_,_,_,_,_,_,_,_\n"+
+								"_,_,_,_,_,_,_,_\n"+
+								"_,_,_,_,_,_,_,_\n"+
+								"_,_,_,_,_,_,_,_\n"+
+								"_,_,_,_,_,_,_,_\n"+
+								"_,_,_,_,_,_,_,_\n"+
+								"_,_,_,_,_,_,_,_";*/
+	private String dummyData = "";
+
 	//GUI
 	public Client(){
 		super("Client: Matrix Addition");
-
 		int height = 800;
 		int width = 600;
 		Dimension frameD = new Dimension(width,height);
 		backgroundPanel = new JPanel();
 		backgroundPanel.setPreferredSize(frameD);
 
+
 		submitBtn = new JButton("SUBMIT");
+
 		addBtn = new JButton("ADD");
 
-		output = new JTextArea();
-		output.setPreferredSize(new Dimension(width,height/5));
-		output.setBackground(Color.gray);
-		output.setBorder(BorderFactory.createTitledBorder("Terminal Output"));
-		
+		output = new JTextArea("Matrix Addition Client Interface\n"+
+								"Version 1.0.0\n"+
+								"Ready...\n",
+								8,55);
+		output.setWrapStyleWord(true);
+		output.setLineWrap(true);
+		output.setForeground(Color.green);
+		output.setBackground(Color.black);
+		outputPanel = new JScrollPane(output,
+								JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+								JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		JScrollBar vSBar = outputPanel.getVerticalScrollBar();
+
+		outputPanel.setBorder(BorderFactory.createTitledBorder("Terminal"+ 
+														"Output"));
+
 		fileInputLabel = new JLabel("Enter File Name: ");
+		fileNotFoundLbl = new JLabel("File Not Found");
+		fileNotFoundLbl.setForeground(Color.red);
 		textPanel = new JPanel();
 		textPanel.setPreferredSize(new Dimension(width,height/10));
-		textPanel.setBackground(Color.red);
+		textPanel.setBackground(Color.lightGray);
 		
 		displayPanel1 = new JPanel();
-		displayPanel1.setPreferredSize(new Dimension(width/3,height/3));
-		displayPanel1.setBackground(Color.yellow);
+		displayPanel1.setPreferredSize(new Dimension(width/3,height/4));
+		displayPanel1.setBackground(Color.lightGray);
 
 		displayPanel2 = new JPanel();
-		displayPanel2.setPreferredSize(new Dimension(width/3,height/3));
+		displayPanel2.setPreferredSize(new Dimension(width/3,height/4));
 		displayPanel2.setBackground(Color.cyan);
 
 		displayPanel3 = new JPanel();
-		displayPanel3.setPreferredSize(new Dimension(width/3,height/3));
+		displayPanel3.setPreferredSize(new Dimension(width/3,height/4));
 		displayPanel3.setBackground(Color.green);
 
 		userInputField = new JTextField(" .txt",25);
-		displayArea1 = new JTextArea("1,1,1\n2,2,2\n3,3,3");
-		
-		//m1.setPreferredSize(new Dimension(50,50));
-		
-		displayArea2 = new JTextArea("1,1,1\n2,2,2\n3,3,3");
-		
-		//m2.setPreferredSize(new Dimension(50,50));
-
-		displayArea3 = new JTextArea("1,1,1\n2,2,2\n3,3,3");
-		
-		//m3.setPreferredSize(new Dimension(50,50));
+		displayArea1 = new JTextArea(dummyData);
+		displayArea1.setFont(new Font("Bookman Old Style", Font.BOLD,19));
+		displayArea2 = new JTextArea(dummyData);
+		displayArea2.setFont(new Font("Algerian", Font.BOLD,19));
+		displayArea3 = new JTextArea(dummyData);
+		displayArea3.setFont(new Font("Broadway", Font.BOLD,19));
 
 		textPanel.add(fileInputLabel);
 		textPanel.add(userInputField);
 		textPanel.add(submitBtn);
-		//addBtn.setVisible(false);
+		addBtn.setVisible(false); //when file is good then visible
 		textPanel.add(addBtn);
+		textPanel.add(fileNotFoundLbl,BorderLayout.PAGE_END);
+		fileNotFoundLbl.setVisible(false); //if file not found visible
 
-		//displayPanel1.add(m1);
 		displayPanel1.add(displayArea1);
 		displayPanel1.setBorder(BorderFactory.createTitledBorder("Matrix A"));
-		//displayPanel2.add(m2);
+
 		displayPanel2.add(displayArea2);
 		displayPanel2.setBorder(BorderFactory.createTitledBorder("Matrix B"));
-		//displayPanel3.add(m3);
+
 		displayPanel3.add(displayArea3);
 		displayPanel3.setBorder(BorderFactory.createTitledBorder("Matrix Sum"));
 
@@ -118,11 +142,10 @@ public class Client extends JFrame{
 		backgroundPanel.add(displayPanel1);
 		backgroundPanel.add(displayPanel2);
 		backgroundPanel.add(displayPanel3);
-		backgroundPanel.add(output);
+		backgroundPanel.add(outputPanel);
 
 		add(backgroundPanel);
 		setSize(700,600);
-		//setSize(frameD);
 		setVisible(true);
 
 	}
